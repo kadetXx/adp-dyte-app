65dac6f24f427f701cd3a1d8ec1828ac2aeef868
kadetXx <techkadet@gmail.com>
2023-02-07 02:23:21 +0100

    feat: add custom colors for meeting ui

--- src/pages/meeting/screens/Call.tsx
+++ src/pages/meeting/screens/Call.tsx
@@ -1,13 +1,14 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useRef } from "react";
 import { useParams, useNavigate } from "react-router-dom";
 import { useMeeting } from "../hooks/useMeeting";
 import { useDyteClient } from "@dytesdk/react-web-core";
-import { DyteMeeting } from "@dytesdk/react-ui-kit";
+import { DyteMeeting, provideDyteDesignSystem } from "@dytesdk/react-ui-kit";
 import { CallParams, ROLE, STORAGE_KEYS } from "../types";
 
 export const Call = () => {
   const params = useParams<CallParams>();
   const navigate = useNavigate();
+  const meetingEl = useRef(null);
 
   const [meeting, initMeeting] = useDyteClient();
 
@@ -45,5 +46,21 @@ export const Call = () => {
       : null;
   }, []);
 
-  return <DyteMeeting meeting={meeting!} />;
+  useEffect(() => {
+    if (!meetingEl.current) return;
+
+    provideDyteDesignSystem(meetingEl.current, {
+      colors: {
+        brand: {
+          300: "#399999",
+          400: "#208c8c",
+          500: "#077F7F",
+          600: "#067272",
+          700: "#066666",
+        },
+      },
+    });
+  }, [meetingEl]);
+
+  return <DyteMeeting meeting={meeting!} ref={meetingEl} />;
 };
